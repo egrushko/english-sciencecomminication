20b76715aac0ffd6e9cbe7aefe8ad024c534a8d0
eepodrezov <egor.podrezov@kirarock.team>
2024-04-15 14:48:47 +0300

    edit coma + footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,8 @@ export const Footer = () =>{
     return(
         <div className="footer">
             <div className="footer-top">
-            <p>2023 Created by Repin Roman</p>
-                
-                <p>|</p> 
                 <p>Site Administrator:<a className='footer-link' href='http://englishforsciences.sfedu.ru/en/staff/p/grushko.html'> Elena I. Grushko</a></p>  
+                <p>2024 Created by Roman Repin & Egor Podrezov</p>
             </div>
         </div>
     )
