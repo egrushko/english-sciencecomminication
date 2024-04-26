a43193eb56f192375a4e876fb57fee8ba8c7da96
eepodrezov <egor.podrezov@kirarock.team>
2024-04-26 18:52:09 +0300

    add yandex metrica, add youtube fullscreen

--- src/components/TasksUtils/Video.js
+++ src/components/TasksUtils/Video.js
@@ -17,7 +17,8 @@ export const Video = ({vid, youtubeSrc, className}) => {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                     referrerpolicy="strict-origin-when-cross-origin" 
                     rel='0'
-                    allowfullscreen></iframe>
+                    allowfullscreen='allowfullscreen'
+                ></iframe>
                 
             </div>
     )}
